523fc604d7494fc347d1abeb496663091b171cf6
HussamAshayer <ashayerhussam@gmail.com>
2024-09-16 02:03:46 +0300

    fuck css

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,367 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-    <main>
-    <div className="container">
-    
-      <div className="item item1">
-        <h2 className="burger-category">BEEF BURGER</h2>
-        {/* <div className="meal-header">
-          <h2 className='regular'>REGULAR</h2>
-          <div className="sizes">
-            <div className="size">S</div>
-            <div className="size">M</div>
-            <div className="size">L</div>
-            <div className="size">Meal</div>
+<main>
+      <div className="container">
+        <div className="item item1">
+          <h2 className="burger-category">BEEF BURGER</h2>
+          <div className="meal-header">
+            <h2 className="regular">REGULAR</h2>
+            <div className="sizes">
+              <div className="size">S</div>
+              <div className="size">M</div>
+              <div className="size">L</div>
+              <div className="size">Meal</div>
+            </div>
           </div>
-        </div> */}
-        <div>
-         
-          <div className="meal-item">
-            <div className="meal">
-              <h4 className="meal-name">REGULAR</h4>
-              <h4 className="meal-name">Cheese Burger</h4>
-              <div className="meal-description">Cheddar,Cheese,Lettuce,<br />Tomato,Onion,Pickles,<br />70's sauce</div>
+          <div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
               </div>
-           
-             <div className="prices">
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-          </div>
-          </div>
-          <div className="meal-item">
-            <div className="meal">
-              <h4 className="meal-name">Cheese Burger</h4>
-              <div className="meal-description">Cheddar,Cheese,Lettuce,<br />Tomato,Onion,Pickles,<br />70's sauce</div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
               </div>
-             <div className="prices">
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-          </div>
-          </div>
-          <div className="meal-item">
-            <div className="meal">
-              <h4 className="meal-name">Cheese Burger</h4>
-              <div className="meal-description">Cheddar,Cheese,Lettuce,<br />Tomato,Onion,Pickles,<br />70's sauce</div>
-              </div>
-             <div className="prices">
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-          </div>
-          </div>
-          <div className="meal-item">
-            <div className="meal">
-              <h4 className="meal-name">Cheese Burger</h4>
-              <div className="meal-description">Cheddar,Cheese,Lettuce,<br />Tomato,Onion,Pickles,<br />70's sauce</div>
-              </div>
-             <div className="prices">
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-          </div>
-          </div>
-          <div className="meal-item">
-            <div className="meal">
-              <h4 className="meal-name">Cheese Burger</h4>
-              <div className="meal-description">Cheddar,Cheese,Lettuce,<br />Tomato,Onion,Pickles,<br />70's sauce</div>
-              </div>
-             <div className="prices">
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-            <div className="price">3.00</div>
-          </div>
-          </div>
-         
-        </div>
-      </div>
-      <div className="item item2">item </div>
-      <div className="item item3">item 3</div>
-      <div className="item item4">item 4</div>
-      <div className="item item5">item 5</div>
-      <div className="item item6">item 6</div>
+            </div>
 
-    </div>
-    
-    
-    
-    </main>
-  )
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+          </div>
+          <div className="meal-item">
+            <div className="meal">
+              <h4 className="meal-name">Blue Cheese Burger</h4>
+              <div className="meal-description">
+                Cheddar,Cheese,Lettuce,
+                <br />
+                Tomato,Onion,Pickles,
+                <br />
+                70's sauce
+              </div>
+            </div>
+            <div className="prices">
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+            </div>
+          </div>
+        </div>
+        <div className="item item2">
+          <img src="image.png" alt="70'S burger" className="logo" />
+           </div>
+        <div className="item item1">
+
+          <div className="meal-header">
+            <h2 className="regular">REGULAR</h2>
+            <div className="sizes">
+              <div className="size">S</div>
+              <div className="size">M</div>
+              <div className="size">L</div>
+              <div className="size">Meal</div>
+            </div>
+          </div>
+          <div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+          </div>
+          <div className="meal-item">
+            <div className="meal">
+              <h4 className="meal-name">Blue Cheese Burger</h4>
+              <div className="meal-description">
+                Cheddar,Cheese,Lettuce,
+                <br />
+                Tomato,Onion,Pickles,
+                <br />
+                70's sauce
+              </div>
+            </div>
+            <div className="prices">
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="item item1">
+
+          <div className="meal-header">
+            <h2 className="regular">REGULAR</h2>
+            <div className="sizes">
+              <div className="size">S</div>
+              <div className="size">M</div>
+              <div className="size">L</div>
+              <div className="size">Meal</div>
+            </div>
+          </div>
+          <div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+          </div>
+          <div className="meal-item">
+            <div className="meal">
+              <h4 className="meal-name">Blue Cheese Burger</h4>
+              <div className="meal-description">
+                Cheddar,Cheese,Lettuce,
+                <br />
+                Tomato,Onion,Pickles,
+                <br />
+                70's sauce
+              </div>
+            </div>
+            <div className="prices">
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="item item5">item 5</div>
+        <div className="item item1">
+
+          <div className="meal-header">
+            <h2 className="regular">REGULAR</h2>
+            <div className="sizes">
+              <div className="size">S</div>
+              <div className="size">M</div>
+              <div className="size">L</div>
+              <div className="size">Meal</div>
+            </div>
+          </div>
+          <div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+            <div className="meal-item">
+              <div className="meal">
+                <h4 className="meal-name">Cheese Burger</h4>
+                <div className="meal-description">
+                  Cheddar,Cheese,Lettuce,
+                  <br />
+                  Tomato,Onion,Pickles,
+                  <br />
+                  70's sauce
+                </div>
+              </div>
+              <div className="prices">
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+                <div className="price">3.00</div>
+              </div>
+            </div>
+          </div>
+          <div className="meal-item">
+            <div className="meal">
+              <h4 className="meal-name">Blue Cheese Burger</h4>
+              <div className="meal-description">
+                Cheddar,Cheese,Lettuce,
+                <br />
+                Tomato,Onion,Pickles,
+                <br />
+                70's sauce
+              </div>
+            </div>
+            <div className="prices">
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+              <div className="price">3.00</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </main>
+  );
 }
 
-export default App
+export default App;
