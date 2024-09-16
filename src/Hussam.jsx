25fc2beaddff4309bd22df7d9ad8b03cecfa4a50
ramez tayem <rameztayem@gmail.com>
2024-09-16 04:05:47 +0300

    4:00am

---
+++ src/Hussam.jsx
@@ -0,0 +1,252 @@
+const Hussam = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="box">
+          <h1 className="box-header">BEEF BURGER</h1>
+          <h2 className="choice">Regular</h2>
+          <p className="size">S</p>
+          <p className="size">M</p>
+          <p className="size">L</p>
+          <p className="size">Meal</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Blue Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+        </div>
+        <div className="logo-box">
+          <img src="image.png" alt="70'S burger" className="logo" />
+        </div>
+        <div className="box">
+          <h1 className="box-header">CHICKEN BURGER</h1>
+          <h2 className="choice">Regular</h2>
+          <p className="size">S</p>
+          <p className="size">M</p>
+          <p className="size">L</p>
+          <p className="size">Meal</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Blue Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+        </div>
+        <div className="box">
+          <h2 className="choice">Regular</h2>
+          <p className="size">S</p>
+          <p className="size">M</p>
+          <p className="size">L</p>
+          <p className="size">Meal</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Blue Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+        </div>
+        <div className="item item5">
+          <div className="wings-header">
+            <h1>WINGS & BONLESS</h1>
+          </div>
+          {/* grid */}
+          <div className="wings-grid">
+            <h4>BBQ.........</h4>
+            <p className="wings-price">3.50</p>
+            <p className="wings-price">5.50</p>
+            <h4>Buffalo.........</h4>
+            <p className="wings-price">3.50</p>
+            <p className="wings-price">5.50</p>
+            <h4>Sweet Chilli.........</h4>
+            <p className="wings-price">3.50</p>
+            <p className="wings-price">5.50</p>
+            <h4>Honey Mustard.........</h4>
+            <p className="wings-price">3.50</p>
+            <p className="wings-price">5.50</p>
+            <h4>70's.........</h4>
+            <p className="wings-price">3.50</p>
+            <p className="wings-price">5.50</p>
+          </div>
+
+          <div className="shrimp-container">
+            <h1 className="shrimp-header">SHRIMPS</h1>
+            <div className="shrimp-items">
+              <p>Sweet Chilli Shrimps ............ 3.75</p>
+              <p>Buffalo Shrimps .................... 3.75</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <h2 className="choice">RECOMMENDED</h2>
+          <p className="size">S</p>
+          <p className="size">M</p>
+          <p className="size">L</p>
+          <p className="size">Meal</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">2.75</p>
+          <p className="price">3.75</p>
+          <p className="price">4.75</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <h2 className="span-choice">CRISPY CHICKEN</h2>
+          <div className="box-meal-desc">
+            <h2>Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">&nbsp;</p>
+          <p className="price">3.75</p>
+          <p className="price">&nbsp;</p>
+          <p className="price">+1.50</p>
+          {/* MEAL DESCRIPTION */}
+          <h2 className="span-choice">CHICKEN BREAST</h2>
+          <div className="box-meal-desc">
+            <h2>Blue Cheese Burger</h2>
+            <p className="meal-desc">
+              Cheddar Cheese Lettuce Tomato Onion Pickeles 70&rsquo;s sauce
+            </p>
+          </div>
+          <p className="price">&nbsp;</p>
+          <p className="price">3.75</p>
+          <p className="price">&nbsp;</p>
+          <p className="price">+1.50</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Hussam;
